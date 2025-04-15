8827a2834a6c97c29cb38ff3da587e24357a0408
Nitansh <nitansh@test.com>
2025-04-15 15:32:24 +0530

    'pink swan king' favicon added

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Units</title>
+    <link rel="shortcut icon" href="{{ asset('kingswan.jpeg') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="icon" href="{{ asset('images/farmone.png') }}" type="image/png">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     @viteReactRefresh
     @vite(['resources/js/app.jsx'])
